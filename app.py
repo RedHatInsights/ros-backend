37a4e4aa7c273ee5529afd3edbae7f6f58214318
kgaikwad <kavitagaikwad103@gmail.com>
2020-11-12 15:39:58 +0530

    enable CORS using flask_cors

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask
-
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app)
 
 
-@app.route("/ping")
+@app.route('/ping')
 def ping():
-    return "Application is running! Ping worked!"
-
+  """Ping API to check application status."""
+  return 'Application is running! Ping worked!'
 
 if __name__ == "__main__":
     app.run(debug=True)
