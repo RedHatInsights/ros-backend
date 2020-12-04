0f2f884ffcb62842b3dc937743840279918f657e
Suraj Patil <patilsuraj767@gmail.com>
2020-12-04 13:12:52 +0530

    Added report date to PerformanceProfile model

--- ros/models.py
+++ ros/models.py
@@ -1,4 +1,4 @@
-import uuid
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.dialects.postgresql import UUID
 
@@ -7,6 +7,7 @@ db = SQLAlchemy()
 
 class PerformanceProfile(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    host_id = db.Column(UUID(as_uuid=True), default=uuid.uuid4)
+    host_id = db.Column(UUID(as_uuid=True), unique=True, nullable=False)
     avg_memory_used = db.Column(db.String(25))
     avg_memory = db.Column(db.String(25))
+    report_date = db.Column(db.DateTime, default=datetime.now())
