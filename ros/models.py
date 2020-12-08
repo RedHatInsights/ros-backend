0f55ff07d8fb00dacbfc34bc51143e885b9c3234
Suraj Patil <patilsuraj767@gmail.com>
2020-12-08 16:01:32 +0530

    Added unique constraint on (inventory_id, report_date)

--- ros/models.py
+++ ros/models.py
@@ -6,8 +6,10 @@ db = SQLAlchemy()
 
 
 class PerformanceProfile(db.Model):
+    __table_args__ = (db.UniqueConstraint('inventory_id', 'report_date'), )
     id = db.Column(db.Integer, primary_key=True)
     inventory_id = db.Column(UUID(as_uuid=True), unique=True, nullable=False)
     performance_record = db.Column(JSONB)
     performance_score = db.Column(JSONB)
-    report_date = db.Column(db.Date, unique=True, default=date.today())
+    report_date = db.Column(db.Date, default=date.today())
+
