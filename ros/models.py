bb127f87e669598ea89f67a965b958ce39164a6f
Suraj Patil <patilsuraj767@gmail.com>
2020-12-08 19:16:59 +0530

    Remove unique key constraint on inventory_id

--- ros/models.py
+++ ros/models.py
@@ -8,7 +8,7 @@ db = SQLAlchemy()
 class PerformanceProfile(db.Model):
     __table_args__ = (db.UniqueConstraint('inventory_id', 'report_date'), )
     id = db.Column(db.Integer, primary_key=True)
-    inventory_id = db.Column(UUID(as_uuid=True), unique=True, nullable=False)
+    inventory_id = db.Column(UUID(as_uuid=True), nullable=False)
     performance_record = db.Column(JSONB)
     performance_score = db.Column(JSONB)
     report_date = db.Column(db.Date, default=date.today())
