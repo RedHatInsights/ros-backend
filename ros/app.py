b6ce21517a00ab69c670ba9e827e4114883c4cfd
Suraj Patil <patilsuraj767@gmail.com>
2020-12-09 15:15:04 +0530

    Changed API from /api/system to /api/systems

--- ros/app.py
+++ ros/app.py
@@ -18,7 +18,7 @@ db.init_app(app)
 # Routes
 api.add_resource(Status, '/api/status')
 api.add_resource(Hosts, '/api/systems')
-api.add_resource(HostDetails, '/api/system/<host_id>')
+api.add_resource(HostDetails, '/api/systems/<host_id>')
 
 if __name__ == "__main__":
     app.run(debug=True)
