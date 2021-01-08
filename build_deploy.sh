3e4de130b55d0d7481bd14ba2ba25fba990a7f19
Suraj Patil <patilsuraj767@gmail.com>
2021-01-08 23:25:25 +0530

    Changed quay repo name

--- build_deploy.sh
+++ build_deploy.sh
@@ -2,7 +2,7 @@
 
 set -exv
 
-IMAGE_NAME="quay.io/cloudservices/resource-optimization"
+IMAGE_NAME="quay.io/cloudservices/ros-backend"
 IMAGE_TAG=$(git rev-parse --short=7 HEAD)
 
 if [[ -z "$QUAY_USER" || -z "$QUAY_TOKEN" ]]; then
