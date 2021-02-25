2586973c4142a9e5b337809feeaa21764e99879b
Suraj Patil <31805557+patilsuraj767@users.noreply.github.com>
2021-02-25 11:58:36 +0000

    Fix API calls of ROS backend (#36)
    * Fix DB queries (schema changes)
    
    (cherry picked from commit 05a26a4cf2a875e28defcb1bbf7c2a0cb28b56a7)
    
    * Fix API calls to ROS
    
    * Change system_id to inventory_id
    
    * Remove facts field.
    
    * Fixed fact field
    
    Co-authored-by: mhornick <mhornick@redhat.com>
    (cherry picked from commit 4e4cf83c821a5856d7a9573aa9c10eb9987017de)

--- ros/api/v0/hosts.py
+++ ros/api/v0/hosts.py
@@ -1,11 +1,11 @@
-from flask import jsonify, make_response
 from flask import request
 from flask_restful import Resource, abort, fields, marshal_with
 
-from ros.lib.models import PerformanceProfile
-from ros.lib.utils import is_valid_uuid
-from ros.lib.host_inventory_interface import fetch_all_hosts_from_inventory
+from ros.lib.models import PerformanceProfile, RhAccount, System, db
+from ros.lib.utils import is_valid_uuid, identity
 from ros.api.common.pagination import build_paginated_system_list_response
+
+from sqlalchemy import func
 
 DEFAULT_HOSTS_PER_REP = 10
 DEFAULT_OFFSET = 0
@@ -13,12 +13,6 @@ DEFAULT_OFFSET = 0
 
 class HostsApi(Resource):
 
-    facts_fields = {
-        'cloud_provider': fields.String,
-        'instance_type': fields.String,
-        'idling_time': fields.String,
-        'io_wait': fields.String
-    }
     display_performance_score_fields = {
         'cpu_score': fields.Integer,
         'memory_score': fields.Integer,
@@ -27,12 +21,16 @@ class HostsApi(Resource):
     hosts_fields = {
         'fqdn': fields.String,
         'display_name': fields.String,
-        'id': fields.Integer,
+        'inventory_id': fields.String,
         'account': fields.String,
         'recommendation_count': fields.Integer,
         'state': fields.String,
         'display_performance_score': fields.Nested(display_performance_score_fields),
-        'facts': fields.Nested(facts_fields)
+        'cloud_provider': fields.String,
+        'instance_type': fields.String
+        # TODO - idling_time, io_wait
+        # 'idling_time': fields.String,
+        # 'io_wait': fields.String
     }
     meta_fields = {
         'count': fields.Integer,
@@ -56,39 +54,47 @@ class HostsApi(Resource):
         limit = int(request.args.get('limit') or DEFAULT_HOSTS_PER_REP)
         offset = int(request.args.get('offset') or DEFAULT_OFFSET)
 
-        auth_key = request.headers.get('X-RH-IDENTITY')
-        if not auth_key:
-            response = make_response(
-                jsonify({"Error": "Authentication token not provided"}), 401)
-            abort(response)
-
-        inv_hosts = fetch_all_hosts_from_inventory(auth_key)
-        inv_host_ids = [host['id'] for host in inv_hosts['results']]
-
+        ident = identity(request)['identity']
         # Note that When using LIMIT, it is important to use an ORDER BY clause
         # that constrains the result rows into a unique order.
         # Otherwise you will get an unpredictable subset of the query's rows.
         # Refer - https://www.postgresql.org/docs/13/queries-limit.html
 
-        query = PerformanceProfile.query.filter(
-            PerformanceProfile.inventory_id.in_(inv_host_ids)).order_by(
-                PerformanceProfile.report_date.desc()).order_by(
-                    PerformanceProfile.id.asc())
+        account_query = db.session.query(RhAccount.id).filter(RhAccount.account == ident['account_number']).subquery()
+        system_query = db.session.query(System.id)\
+            .filter(System.account_id.in_(account_query)).subquery()
+
+        last_reported = (
+            db.session.query(PerformanceProfile.system_id, func.max(PerformanceProfile.report_date).label('max_date')
+                             )
+            .filter(PerformanceProfile.system_id.in_(system_query))
+            .group_by(PerformanceProfile.system_id)
+            .subquery()
+        )
+
+        query = (
+            db.session.query(PerformanceProfile, System, RhAccount)
+            .join(last_reported, (last_reported.c.max_date == PerformanceProfile.report_date) &
+                  (PerformanceProfile.system_id == last_reported.c.system_id))
+            .join(System, System.id == last_reported.c.system_id)
+            .join(RhAccount, RhAccount.id == System.account_id)
+            .order_by(PerformanceProfile.system_id.asc())
+        )
+
         count = query.count()
         query = query.limit(limit).offset(offset)
         query_results = query.all()
 
         hosts = []
-        for profile in query_results:
-            if len(list(filter(lambda host: host['id'] == str(profile.inventory_id), hosts))):
-                continue
-            else:
-                host = list(filter(lambda host: host['id'] == str(profile.inventory_id), inv_hosts['results']))[0]
-                host['id'] = profile.__dict__['id']
-                host['recommendation_count'] = 5
-                host['state'] = 'Undersized'
-                host['display_performance_score'] = profile.display_performance_score
-                hosts.append(host)
+        system_columns = ['inventory_id', 'fqdn', 'display_name', 'instance_type', 'cloud_provider']
+        for row in query_results:
+            system_dict = row.System.__dict__
+            host = {skey: system_dict[skey] for skey in system_columns}
+            host['recommendation_count'] = 5
+            host['state'] = 'Undersized'
+            host['account'] = row.RhAccount.account
+            host['display_performance_score'] = row.PerformanceProfile.display_performance_score
+            hosts.append(host)
 
         return build_paginated_system_list_response(
             limit, offset, hosts, count
@@ -105,11 +111,17 @@ class HostDetailsApi(Resource):
     @marshal_with(profile_fields)
     def get(self, host_id):
         if not is_valid_uuid(host_id):
-            abort(404, message='Invalid host_id,'
-                               ' Id should be in form of UUID4')
+            abort(404, message='Invalid host_id, Id should be in form of UUID4')
 
-        profile = PerformanceProfile.query.filter_by(
-                  inventory_id=host_id).first()
+        ident = identity(request)['identity']
+        account_query = db.session.query(RhAccount.id).filter(RhAccount.account == ident['account_number']).subquery()
+        system_query = db.session.query(System.id) \
+            .filter(System.account_id.in_(account_query)).filter(System.inventory_id == host_id).subquery()
+
+        profile = PerformanceProfile.query.filter(
+            PerformanceProfile.system_id.in_(system_query)
+        ).order_by(PerformanceProfile.report_date.desc()).first()
+
         if profile:
             record = {}
             record['display_performance_score'] = profile.display_performance_score
--- ros/lib/utils.py
+++ ros/lib/utils.py
@@ -1,4 +1,8 @@
 import uuid
+import base64
+import json
+from flask import jsonify, make_response
+from flask_restful import abort
 
 
 def is_valid_uuid(val):
@@ -25,3 +29,12 @@ def get_or_create(session, model, keys, **kwargs):
         session.add(instance)
         session.flush()
     return instance
+
+
+def identity(request):
+    ident = request.headers.get('X-RH-IDENTITY')
+    if not ident:
+        response = make_response(
+            jsonify({"Error": "Authentication token not provided"}), 401)
+        abort(response)
+    return json.loads(base64.b64decode(ident))
