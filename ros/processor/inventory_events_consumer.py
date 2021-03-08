16cb729c21677fa3e4dd7eca7a24780d07a2cd43
Suraj Patil <31805557+patilsuraj767@users.noreply.github.com>
2021-03-08 08:56:14 +0000

    Implement insights core inside ROS (#42)
    * Implement insights core inside ROS
    
    * Remove default

--- ros/processor/inventory_events_consumer.py
+++ ros/processor/inventory_events_consumer.py
@@ -1,15 +1,12 @@
 import json
 import logging
-import requests
 import datetime
-from http import HTTPStatus
-import tarfile
-from io import BytesIO
 from confluent_kafka import Consumer, KafkaException
 from ros.lib.config import INSIGHTS_KAFKA_ADDRESS, INVENTORY_EVENTS_TOPIC, GROUP_ID
 from ros.lib.app import app, db
 from ros.lib.models import PerformanceProfile, RhAccount, System
 from ros.lib.utils import get_or_create
+from ros.processor.process_archive import get_performance_profile
 
 logging.basicConfig(
     level='INFO',
@@ -109,8 +106,7 @@ class InventoryEventsConsumer:
     def process_system_details(self, msg):
         """ Store new system information (stale, stale_warning timestamp) and return internal DB id"""
         host = msg['host']
-        insights_report_tar = self._download_report(msg['platform_metadata']['url'])
-        performance_record = self._extract_performance_record(insights_report_tar)
+        performance_record = get_performance_profile(msg['platform_metadata']['url'])
         if performance_record:
             performance_score = self._calculate_performance_score(performance_record)
             with app.app_context():
@@ -125,7 +121,8 @@ class InventoryEventsConsumer:
                     inventory_id=host['id'],
                     display_name=host['display_name'],
                     fqdn=host['fqdn'],
-                    cloud_provider=host['system_profile']['cloud_provider']
+                    cloud_provider=host['system_profile']['cloud_provider'],
+                    instance_type=performance_record.get('instance_type')
                 )
 
                 get_or_create(
@@ -140,42 +137,6 @@ class InventoryEventsConsumer:
                 db.session.commit()
                 LOG.info("Refreshed system %s (%s) belonging to account: %s (%s)",
                          system.inventory_id, system.id, account.account, account.id)
-
-    def _download_report(self, report_url):
-        download_response = requests.get(report_url)
-        if download_response.status_code != HTTPStatus.OK:
-            LOG.error("Unable to download the report. ERROR - %s", download_response.reason)
-        return download_response.content
-
-    def _extract_performance_record(self, report_tar):
-        tar = tarfile.open(fileobj=BytesIO(report_tar), mode='r:*')
-        files = tar.getmembers()
-        performance_record = {}
-        spec_count = 0
-        for file in files:
-            if 'data/insights_commands/pmlogsummary' in file.name:
-                spec_count += 1
-                file_data = self._extract_spec_file_data(tar, file)
-                lines = file_data.split('\n')
-                for line in lines:
-                    key, value, _ = line.split(None, 2)
-                    performance_record[key] = value
-
-            if 'data/insights_commands/lscpu' in file.name:
-                spec_count += 1
-                file_data = self._extract_spec_file_data(tar, file)
-                lines = file_data.split('\n')
-                for line in lines:
-                    if line.startswith("CPU(s):"):
-                        cpus = line.split()[1]
-                        performance_record['total_cpus'] = cpus
-
-            if spec_count == 2:
-                return performance_record
-
-    def _extract_spec_file_data(self, tar, file):
-        extracted_file = tar.extractfile(file)
-        return extracted_file.read().decode('utf-8')
 
     def _calculate_performance_score(self, performance_record):
         memory_score = (float(performance_record['mem.util.used']) / float(performance_record['mem.physmem'])) * 100
