ccd85b2ad4f15e8e92921e5322bbcde2f2de01ac
Suraj Patil <patilsuraj767@gmail.com>
2021-03-10 12:37:24 +0530

    Cleanup code related to V1 architecture
    (cherry picked from commit 826b36fdba7bdc3f2f0c71f373c0719d0ccbcdce)

--- ros/processor/main.py
+++ ros/processor/main.py
@@ -1,11 +1,5 @@
-from ros.processor.report_processor import ReportProcessor
 from ros.processor.inventory_events_consumer import InventoryEventsConsumer
 import threading
-
-
-def report_processor():
-    processor = ReportProcessor()
-    processor.init_consumer()
 
 
 def events_processor():
@@ -15,10 +9,7 @@ def events_processor():
 
 if __name__ == "__main__":
     # Start processing in 2 different threads
-    reports = threading.Thread(name='report-processor', target=report_processor)
     events = threading.Thread(name='events-processor', target=events_processor)
-    reports.start()
     events.start()
     # Wait for threads to finish
-    reports.join()
     events.join()
