a46560dcf4317035b24bc865ab1d54b9aff40316
Suraj Patil <patilsuraj767@gmail.com>
2021-06-10 10:54:57 +0530

    Change timezone to UTC

--- ros/processor/garbage_collector.py
+++ ros/processor/garbage_collector.py
@@ -1,6 +1,6 @@
 from ros.lib.app import app, db
 from ros.lib.models import PerformanceProfile
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from ros.lib.config import GARBAGE_COLLECTION_INTERVAL, DAYS_UNTIL_STALE, get_logger
 import time
 
@@ -15,7 +15,10 @@ class GarbageCollector():
     def remove_outdated_data(self):
         with app.app_context():
             results = db.session.query(PerformanceProfile).filter(
-                PerformanceProfile.report_date < datetime.today() - timedelta(days=DAYS_UNTIL_STALE)).delete()
+                PerformanceProfile.report_date < (
+                    datetime.now(timezone.utc) - timedelta(
+                        days=DAYS_UNTIL_STALE)
+                )).delete()
             db.session.commit()
             if results:
                 LOG.info("Deleted %s performance profiles older than %d days", results, DAYS_UNTIL_STALE)
