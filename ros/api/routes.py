e6aec063e3fecfdfde357ee14beeb791b585a839
kgaikwad <kavitagaikwad103@gmail.com>
2021-09-17 14:42:45 +0530

    RHIROS-270 - new is_configured API to check ROS systems exist (#104)
    (cherry picked from commit 666be825ca46a5a9f921ca281ad5a46cb8870b0d)

--- ros/api/routes.py
+++ ros/api/routes.py
@@ -1,4 +1,4 @@
-from .v0.hosts import HostsApi, HostDetailsApi, HostHistoryApi
+from .v0.hosts import HostsApi, HostDetailsApi, HostHistoryApi, IsROSConfiguredApi
 from .v0.recommendation_ratings import RecommendationRatingsApi
 from .v0.recommendations import RecommendationsApi
 from .v0.openapi_spec import OpenAPISpec
@@ -8,6 +8,7 @@ from .v0.status import Status
 # Initialize Routes
 def initialize_routes(api):
     api.add_resource(Status, '/api/ros/v0/status')
+    api.add_resource(IsROSConfiguredApi, '/api/ros/v0/is_configured')
     api.add_resource(HostsApi, '/api/ros/v0/systems')
     api.add_resource(HostHistoryApi, '/api/ros/v0/systems/<host_id>/history')
     api.add_resource(RecommendationsApi, '/api/ros/v0/systems/<host_id>/recommendations')
