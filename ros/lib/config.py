056cab3362376ddec02d3c81057312f45eb41ebf
Aditi Puntambekar <apuntamb@redhat.com>
2021-09-21 15:53:16 +0530

    RHIROS #188 Migrate to Clowder (#86)
    * RHIROS #188 Migrate to Clowder
    
    * added ENABLE_RBAC flag and renamed to clowdapp.yaml
    
    * organised configs for clowder and legacy
    
    * fixed ports and updated clowdapp
    
    * Update Makefile

--- ros/lib/config.py
+++ ros/lib/config.py
@@ -22,37 +22,42 @@ def get_logger(name):
     return logging.getLogger(name)
 
 
-DB_USER = os.getenv("ROS_DB_USER", "postgres")
-DB_PASSWORD = os.getenv("ROS_DB_PASS", "postgres")
-DB_HOST = os.getenv("ROS_DB_HOST", "localhost")
-DB_PORT = os.getenv("ROS_DB_PORT", "15432")
-DB_NAME = os.getenv("ROS_DB_NAME", "postgres")
+LOG = logging.getLogger(__name__)
+CLOWDER_ENABLED = True if os.getenv("CLOWDER_ENABLED", default="False").lower() in ["true", "t", "yes", "y"] else False
+
+if CLOWDER_ENABLED:
+    LOG.info("Using Clowder Operator...")
+    from app_common_python import LoadedConfig, KafkaTopics
+    DB_NAME = LoadedConfig.database.name
+    DB_USER = LoadedConfig.database.username
+    DB_PASSWORD = LoadedConfig.database.password
+    DB_HOST = LoadedConfig.database.hostname
+    DB_PORT = LoadedConfig.database.port
+    INSIGHTS_KAFKA_ADDRESS = LoadedConfig.kafka.brokers[0].hostname + ":" + str(LoadedConfig.kafka.brokers[0].port)
+    INVENTORY_EVENTS_TOPIC = KafkaTopics["platform.inventory.events"].name
+    ENGINE_RESULT_TOPIC = KafkaTopics["platform.engine.results"].name
+else:
+    DB_NAME = os.getenv("ROS_DB_NAME", "postgres")
+    DB_USER = os.getenv("ROS_DB_USER", "postgres")
+    DB_PASSWORD = os.getenv("ROS_DB_PASS", "postgres")
+    DB_HOST = os.getenv("ROS_DB_HOST", "localhost")
+    DB_PORT = os.getenv("ROS_DB_PORT", "15432")
+    INSIGHTS_KAFKA_HOST = os.getenv("INSIGHTS_KAFKA_HOST", "localhost")
+    INSIGHTS_KAFKA_PORT = os.getenv("INSIGHTS_KAFKA_PORT", "9092")
+    INSIGHTS_KAFKA_ADDRESS = f"{INSIGHTS_KAFKA_HOST}:{INSIGHTS_KAFKA_PORT}"
+    INVENTORY_EVENTS_TOPIC = os.getenv("INVENTORY_EVENTS_TOPIC", "platform.inventory.events")
+    ENGINE_RESULT_TOPIC = os.getenv("ENGINE_RESULT_TOPIC", "platform.engine.results")
+
 DB_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}"\
                 f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-
 GROUP_ID = os.getenv('GROUP_ID', 'resource-optimization')
-
-INVENTORY_HOST = os.getenv("INVENTORY_HOST", "localhost")
-INVENTORY_PORT = os.getenv("INVENTORY_PORT", "8081")
-INVENTORY_URL = f"http://{INVENTORY_HOST}:{INVENTORY_PORT}"
-INVENTORY_EVENTS_TOPIC = os.getenv("INVENTORY_EVENTS_TOPIC", "platform.inventory.events")
-
-ENGINE_RESULT_TOPIC = os.getenv("ENGINE_RESULT_TOPIC", "platform.engine.results")
-
-INSIGHTS_KAFKA_HOST = os.getenv("INSIGHTS_KAFKA_HOST", "localhost")
-INSIGHTS_KAFKA_PORT = os.getenv("INSIGHTS_KAFKA_PORT", "9092")
-INSIGHTS_KAFKA_ADDRESS = f"{INSIGHTS_KAFKA_HOST}:{INSIGHTS_KAFKA_PORT}"
-
 PATH_PREFIX = os.getenv("PATH_PREFIX", "/api/")
 APP_NAME = os.getenv("APP_NAME", "ros")
-
 INSIGHTS_EXTRACT_LOGLEVEL = os.getenv("INSIGHTS_EXTRACT_LOGLEVEL", "ERROR")
-
 RBAC_HOST = os.getenv("RBAC_HOST", "localhost")
 RBAC_PORT = os.getenv("RBAC_PORT", "8114")
 RBAC_SVC_URL = os.getenv("RBAC_SVC_URL", f"http://{RBAC_HOST}:{RBAC_PORT}/")
 ENABLE_RBAC = str_to_bool(os.getenv("ENABLE_RBAC", "True"))
-
 # Time interval after which garbage collector is involved to check for outdated data.
 GARBAGE_COLLECTION_INTERVAL = os.getenv("GARBAGE_COLLECTION_INTERVAL", 86400)
 # Number of days after which data is considered to be outdated.
