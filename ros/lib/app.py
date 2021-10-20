5ed48690cbc12eba4faf507121f2abb2f185aa9f
kgaikwad <kavitagaikwad103@gmail.com>
2021-10-20 10:55:55 +0530

    RHIROS 133 - Integrate Prometheus and Grafana  (#113)
    * RHIROS 133 - Integrate Prometheus and Grafana
    
    * RHIROS_133 - Grafana-Prometheus custom metrics
    
    * add pyyaml
    
    * fixed metric description
    
    Co-authored-by: Aditi Puntambekar <apuntamb@redhat.com>
    (cherry picked from commit 1368db923f58e1ef815b08def5996bcc04266a7a)

--- ros/lib/app.py
+++ ros/lib/app.py
@@ -4,12 +4,14 @@ from .config import DB_URI
 from flask import request
 from .rbac_interface import ensure_has_permission
 from .config import get_logger
+from prometheus_flask_exporter import RESTfulPrometheusMetrics
 
 # Since we're using flask_sqlalchemy, we must create the flask app in both processor and web api
 app = Flask(__name__)
 # Initalize database connection
 app.config['SQLALCHEMY_DATABASE_URI'] = DB_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+metrics = RESTfulPrometheusMetrics.for_app_factory(defaults_prefix='ros')
 db.init_app(app)
 
 
