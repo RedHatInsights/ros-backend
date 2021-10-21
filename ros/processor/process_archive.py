6e61e6a30113c661f773a04596d4f835b9a1537f
Aditi Puntambekar <apuntamb@redhat.com>
2021-10-21 21:23:44 +0530

    Refs #RHIROS 243 - Update processors to handle empty pmlogsummary data

--- ros/processor/process_archive.py
+++ ros/processor/process_archive.py
@@ -21,32 +21,35 @@ dr.log.setLevel(INSIGHTS_EXTRACT_LOGLEVEL)
 prefix = "ARCHIVE PROCESSOR"
 
 
-@rule(PmLogSummary, LsCPU, [AWSInstanceIdDoc, AzureInstanceType])
-def performance_profile(pmlog_summary, lscpu, aws_instance_id, azure_instance_type):
-    profile = {}
-    performance_metrics = [
-        'mem.physmem',
-        'mem.util.used',
-        'kernel.all.cpu.user',
-        'kernel.all.cpu.sys',
-        'kernel.all.cpu.nice',
-        'kernel.all.cpu.steal',
-        'kernel.all.cpu.idle',
-        'kernel.all.cpu.wait.total',
-        'disk.all.total',
-        'mem.util.cached',
-        'mem.util.bufmem',
-        'mem.util.free'
-        ]
-    profile["total_cpus"] = int(lscpu.info.get('CPUs'))
-    if aws_instance_id:
-        profile["instance_type"] = aws_instance_id.get('instanceType')
-    elif azure_instance_type:
-        profile["instance_type"] = azure_instance_type.raw
+@rule(LsCPU, [AWSInstanceIdDoc, AzureInstanceType], optional=[PmLogSummary])
+def performance_profile(lscpu, aws_instance_id, azure_instance_type, pmlog_summary):
+    if pmlog_summary is not None:
+        profile = {}
+        performance_metrics = [
+            'mem.physmem',
+            'mem.util.used',
+            'kernel.all.cpu.user',
+            'kernel.all.cpu.sys',
+            'kernel.all.cpu.nice',
+            'kernel.all.cpu.steal',
+            'kernel.all.cpu.idle',
+            'kernel.all.cpu.wait.total',
+            'disk.all.total',
+            'mem.util.cached',
+            'mem.util.bufmem',
+            'mem.util.free'
+            ]
+        profile["total_cpus"] = int(lscpu.info.get('CPUs'))
+        if aws_instance_id:
+            profile["instance_type"] = aws_instance_id.get('instanceType')
+        elif azure_instance_type:
+            profile["instance_type"] = azure_instance_type.raw
+        else:
+            profile["instance_type"] = None
+        for i in performance_metrics:
+            profile[i] = _.get(pmlog_summary, f'{i}.val')
     else:
-        profile["instance_type"] = None
-    for i in performance_metrics:
-        profile[i] = _.get(pmlog_summary, f'{i}.val')
+        profile = {}
 
     metadata_response = make_metadata()
     metadata_response.update(profile)
