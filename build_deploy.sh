aea71a0714999c3033279650b8490b729d2219b6
Victor M <Victoremepunto@users.noreply.github.com>
2021-11-12 14:55:49 +0100

    Add additional tags for QA and CI (#128)

--- build_deploy.sh
+++ build_deploy.sh
@@ -4,6 +4,7 @@ set -exv
 
 IMAGE_NAME="quay.io/cloudservices/ros-backend"
 IMAGE_TAG=$(git rev-parse --short=7 HEAD)
+ADDITIONAL_TAGS="qa latest"
 
 if [[ -z "$QUAY_USER" || -z "$QUAY_TOKEN" ]]; then
     echo "QUAY_USER and QUAY_TOKEN must be set"
@@ -21,3 +22,8 @@ docker --config="$DOCKER_CONF" login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
 docker --config="$DOCKER_CONF" login -u="$RH_REGISTRY_USER" -p="$RH_REGISTRY_TOKEN" registry.redhat.io
 docker --config="$DOCKER_CONF" build -t "${IMAGE_NAME}:${IMAGE_TAG}" .
 docker --config="$DOCKER_CONF" push "${IMAGE_NAME}:${IMAGE_TAG}"
+
+for ADDITIONAL_TAG in $ADDITIONAL_TAGS; do
+    docker --config="$DOCKER_CONF" tag "${IMAGE_NAME}:${IMAGE_TAG}" "${IMAGE_NAME}:${ADDITIONAL_TAG}"
+    docker --config="$DOCKER_CONF" push "${IMAGE_NAME}:${ADDITIONAL_TAG}"
+done
