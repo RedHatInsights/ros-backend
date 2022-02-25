8fce9dbb848f2b8ea504e56cf8cc4a9db8f80a37
Sagnik Dutta <sdutta@redhat.com>
2022-02-25 15:44:07 +0530

    Tidied up secret value inits

--- ros/lib/config.py
+++ ros/lib/config.py
@@ -41,7 +41,13 @@ if CLOWDER_ENABLED:
         if endpoint.app == "rbac":
             RBAC_SVC_URL = f"http://{endpoint.hostname}:{endpoint.port}"
             break
-    CW_ENABLED = True if LoadedConfig.Logging.Cloudwatch else False  # CloudWatch/Kibana Logging
+    CW_ENABLED = True if LoadedConfig.logging.cloudwatch else False  # CloudWatch/Kibana Logging
+    if CW_ENABLED is True:
+        # Available only in k8s namespace, through an app-interface automation
+        AWS_ACCESS_KEY_ID = LoadedConfig.logging.cloudwatch.accessKeyId
+        AWS_SECRET_ACCESS_KEY = LoadedConfig.logging.cloudwatch.secretAccessKey
+        AWS_REGION_NAME = LoadedConfig.logging.cloudwatch.region
+        AWS_LOG_GROUP = LoadedConfig.logging.cloudwatch.logGroup
 
 else:
     DB_NAME = os.getenv("ROS_DB_NAME", "postgres")
@@ -58,7 +64,12 @@ else:
     RBAC_HOST = os.getenv("RBAC_HOST", "localhost")
     RBAC_PORT = os.getenv("RBAC_PORT", "8114")
     RBAC_SVC_URL = os.getenv("RBAC_SVC_URL", f"http://{RBAC_HOST}:{RBAC_PORT}/")
-    CW_ENABLED = os.getenv("CW_ENABLED", False)  # CloudWatch/Kibana Logging
+    CW_ENABLED = os.getenv("CW_ENABLED", False)
+    if CW_ENABLED is True:
+        AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID", None)
+        AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY", None)
+        AWS_REGION_NAME = os.getenv("AWS_REGION_NAME", None)
+        AWS_LOG_GROUP = os.getenv("AWS_LOG_GROUP", None)
 
 DB_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}"\
                 f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
@@ -72,10 +83,3 @@ GARBAGE_COLLECTION_INTERVAL = os.getenv("GARBAGE_COLLECTION_INTERVAL", 86400)
 # Number of days after which data is considered to be outdated.
 DAYS_UNTIL_STALE = os.getenv("DAYS_UNTIL_STALE", 14)
 INSTANCE_PRICE_UNIT = 'USD/hour'
-
-if CW_ENABLED is True:
-    # Available only in k8s namespace, through an app-interface automation
-    AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID", None)
-    AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY", None)
-    AWS_REGION_NAME = os.getenv("AWS_REGION_NAME", None)
-    AWS_LOG_GROUP = os.getenv("AWS_LOG_GROUP", None)
