f831ef26b7d007a0141259604b4046acb76a6119
Sagnik Dutta <sdutta@redhat.com>
2022-02-28 21:07:00 +0530

    Removed assignments of environment constants to variables

--- ros/lib/cw_logging.py
+++ ros/lib/cw_logging.py
@@ -27,29 +27,25 @@ module_prefix = 'CLOUDWATCH LOGGING'
 
 def commence_cw_log_streaming(stream_name):
     logger = get_logger(__name__)
-    key_id = AWS_ACCESS_KEY_ID
-    secret_key = AWS_SECRET_ACCESS_KEY
-    region_name = AWS_REGION_NAME
-    log_group = AWS_LOG_GROUP
 
     if CW_ENABLED is False:
         logger.warning(f"{module_prefix} - Disabled")
         return
 
-    if all((key_id, secret_key, region_name, log_group)) is False:
-        logger.error(f"{module_prefix} - Insufficient secret values")
+    if all((AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION_NAME, AWS_LOG_GROUP)) is False:
+        logger.error(f"{module_prefix} - Insufficient constant values")
         return
 
     try:
         boto3_session = Session(
-            aws_access_key_id=key_id,
-            aws_secret_access_key=secret_key,
-            region_name=region_name
+            aws_access_key_id=AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+            region_name=AWS_REGION_NAME
         )
 
         watchtower_handler = watchtower.CloudWatchLogHandler(
             boto3_session=boto3_session,
-            log_group=log_group,
+            log_group=AWS_LOG_GROUP,
             stream_name=stream_name
         )
     except ClientError as e:
