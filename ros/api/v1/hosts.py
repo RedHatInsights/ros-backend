5250bc4c3255d2b7e72f867a2f8a44b14d5914ad
kgaikwad <kavitagaikwad103@gmail.com>
2022-03-23 13:10:49 +0530

    RHIROS-534 - enable sorting on os column

--- ros/api/v1/hosts.py
+++ ros/api/v1/hosts.py
@@ -1,4 +1,4 @@
-from sqlalchemy import func, asc, desc
+from sqlalchemy import func, asc, desc, nullslast, nullsfirst
 from sqlalchemy.types import Float
 from flask import request
 from flask_restful import Resource, abort, fields, marshal_with
@@ -216,6 +216,14 @@ class HostsApi(Resource):
         if order_method == 'state':
             return (sort_order(System.state),
                     asc(PerformanceProfile.system_id),)
+
+        if order_method == 'os':
+            nulls_method = nullsfirst if order_how == 'asc' else nullslast
+            return (
+                nulls_method(sort_order(System.operating_system['name'])),
+                sort_order(System.operating_system['major']),
+                sort_order(System.operating_system['minor'])
+            )
 
         abort(403, message="Unexpected sort method {}".format(order_method))
         return None
