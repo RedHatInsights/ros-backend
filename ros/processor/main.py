dfc01ffd154ddd6e0c6e71e8676684105f3ff036
Suraj Patil <patilsuraj767@gmail.com>
2022-05-06 14:55:55 +0530

    Added monitoring process for openshift props

--- ros/processor/main.py
+++ ros/processor/main.py
@@ -1,3 +1,5 @@
+from http.server import HTTPServer
+from ros.lib.utils import PROCESSOR_INSTANCES, MonitoringHandler
 from ros.lib.cw_logging import commence_cw_log_streaming
 from ros.processor.inventory_events_consumer import InventoryEventsConsumer
 from ros.processor.insights_engine_result_consumer import InsightsEngineResultConsumer
@@ -9,17 +11,28 @@ from ros.lib.config import METRICS_PORT
 
 def process_engine_results():
     processor = InsightsEngineResultConsumer()
+    processor.processor_name = 'process-engine-results'
+    PROCESSOR_INSTANCES.append(processor)
     processor.run()
 
 
 def events_processor():
     processor = InventoryEventsConsumer()
+    processor.processor_name = 'events-processor'
+    PROCESSOR_INSTANCES.append(processor)
     processor.run()
 
 
 def garbage_collector():
     collector = GarbageCollector()
+    collector.processor_name = 'garbage-collector'
+    PROCESSOR_INSTANCES.append(collector)
     collector.run()
+
+
+def thread_monitor():
+    server = HTTPServer(('', 8000), MonitoringHandler)
+    server.serve_forever()
 
 
 if __name__ == "__main__":
@@ -28,9 +41,11 @@ if __name__ == "__main__":
     engine_results = threading.Thread(name='process-engine-results', target=process_engine_results)
     events = threading.Thread(name='events-processor', target=events_processor)
     collector = threading.Thread(name='garbage-collector', target=garbage_collector)
+    threadmonitor = threading.Thread(name='thread-monitor', target=thread_monitor)
     events.start()
     engine_results.start()
     collector.start()
+    threadmonitor.start()
     start_http_server(int(METRICS_PORT))
     # Wait for threads to finish
     events.join()
