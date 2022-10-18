2579cbe0952f8917a6e8ad182296c194a052fce8
kgaikwad <kavitagaikwad103@gmail.com>
2022-10-19 00:54:51 +0530

    Fixes #RHIROS-828 - add display_name under context for consistency (#268)

--- ros/processor/event_producer.py
+++ ros/processor/event_producer.py
@@ -23,7 +23,8 @@ def new_suggestion_event(host, platform_metadata, previous_state, current_state,
         "org_id": org_id,
         "context": {
             "event_name": "New suggestion",
-            "systems_with_suggestions": systems_with_suggestions
+            "systems_with_suggestions": systems_with_suggestions,
+            "display_name": host.get('display_name')
         },
         "events": [
             {
