0069b36e04fe131f72f1a3b608a27107e5910e71
Amit Upadhye <upadhyeammit@gmail.com>
2022-11-28 18:35:03 +0530

    Get the processor and api ports from env vars if available (#277)

--- ros/lib/config.py
+++ ros/lib/config.py
@@ -122,3 +122,5 @@ GARBAGE_COLLECTION_INTERVAL = int(
 DAYS_UNTIL_STALE = int(os.getenv("DAYS_UNTIL_STALE", '45'))
 INSTANCE_PRICE_UNIT = 'USD/hour'
 CW_LOGGING_FORMAT = '%(asctime)s - %(levelname)s  - %(funcName)s - %(message)s'
+ROS_PROCESSOR_PORT = int(os.getenv("ROS_PROCESSOR_PORT", "8000"))
+ROS_API_PORT = int(os.getenv("ROS_API_PORT", "8000"))
