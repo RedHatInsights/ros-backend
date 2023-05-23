df14698a2e07d8f727dfb37f30c3d44b43635e07
Amit Upadhye <upadhyeammit@gmail.com>
2023-05-23 12:14:16 +0530

    Fixes #RHIROS-1142 - Migrate to poetry

--- ros/processor/insights_engine_consumer.py
+++ ros/processor/insights_engine_consumer.py
@@ -96,7 +96,7 @@ class InsightsEngineConsumer:
             )
             reports = []
             if msg["results"]["reports"] \
-                    and type(msg["results"]["reports"]) == list:
+                    and isinstance(msg["results"]["reports"], list):
                 reports = msg["results"]["reports"]
             ros_reports = [
                 report for report in reports
