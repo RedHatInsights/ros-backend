8986925a88aa902b9bc580c2719a1f5c1fa899e3
Amit Upadhye <upadhyeammit@gmail.com>
2023-07-27 21:09:41 +0530

    Rename method and reuse request

--- ros/api/common/add_group_filter.py
+++ ros/api/common/add_group_filter.py
@@ -1,7 +1,8 @@
 from ros.lib.models import System
+from flask_restful import request
 
 
-def include_group_filter(request, query):
+def group_filtered_query(query):
     if hasattr(request, "host_groups"):
         if len(request.host_groups) > 1 or (len(request.host_groups) == 0 and request.host_groups[0] is not None):
             query = query.filter(System.groups[0]['id'].astext.in_(request.host_groups) | (System.groups == '[]'))
