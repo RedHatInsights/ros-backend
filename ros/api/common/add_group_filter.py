89a0c62255589d65d4e4ea3c135362a01856390e
Amit Upadhye <upadhyeammit@gmail.com>
2023-07-28 13:53:13 +0530

    Remove None from group list

--- ros/api/common/add_group_filter.py
+++ ros/api/common/add_group_filter.py
@@ -4,8 +4,9 @@ from flask_restful import request
 
 def group_filtered_query(query):
     if hasattr(request, "host_groups"):
-        if len(request.host_groups) > 1 or (len(request.host_groups) == 0 and request.host_groups[0] is not None):
-            query = query.filter(System.groups[0]['id'].astext.in_(request.host_groups) | (System.groups == '[]'))
+        host_groups = [gid for gid in request.host_groups if gid is not None]
+        if len(host_groups) > 1:
+            query = query.filter(System.groups[0]['id'].astext.in_(host_groups) | (System.groups == '[]'))
         else:
             query = query.filter((System.groups == '[]'))
     return query
