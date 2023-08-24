4e4c259ef74491dfac4b30320cd59cd9dd50dd5d
Amit Upadhye <upadhyeammit@gmail.com>
2023-08-24 19:52:54 +0530

    Refs #RHIROS-1266 - fix condition to use group query

--- ros/api/common/add_group_filter.py
+++ ros/api/common/add_group_filter.py
@@ -1,11 +1,11 @@
 from ros.lib.models import System
-from flask_restful import request
+from flask import request
 
 
 def group_filtered_query(query):
     if hasattr(request, "host_groups"):
         host_groups = [gid for gid in request.host_groups if gid is not None]
-        if len(host_groups) > 1:
+        if len(host_groups) >= 1:
             query = query.filter(System.groups[0]['id'].astext.in_(host_groups) | (System.groups == '[]'))
         else:
             query = query.filter((System.groups == '[]'))
