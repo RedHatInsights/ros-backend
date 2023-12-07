06a23a96c501f2690b48daf9431f2e26b285ffde
Amit Upadhye <upadhyeammit@gmail.com>
2023-12-07 15:35:19 +0530

    Fixes #RHINENG-5028 - support multiple kafka servers

--- ros/lib/consume.py
+++ ros/lib/consume.py
@@ -1,15 +1,13 @@
 from confluent_kafka import Consumer
-from ros.lib.config import INSIGHTS_KAFKA_ADDRESS, GROUP_ID, kafka_auth_config
+from ros.lib.config import kafka_auth_config, GROUP_ID
 
 
 def init_consumer(kafka_topic):
     connection_object = {
-            'group.id': GROUP_ID,
-            'bootstrap.servers': INSIGHTS_KAFKA_ADDRESS,
-            'enable.auto.commit': False
+        'group.id': GROUP_ID,
+        'enable.auto.commit': False
     }
     consumer = Consumer(kafka_auth_config(connection_object))
     # Subscribe to topic
     consumer.subscribe([kafka_topic])
-
     return consumer
--- ros/lib/produce.py
+++ ros/lib/produce.py
@@ -1,13 +1,7 @@
 from confluent_kafka import Producer
-from ros.lib.config import INSIGHTS_KAFKA_ADDRESS, GROUP_ID, kafka_auth_config
+from ros.lib.config import kafka_auth_config
 
 
 def init_producer():
-    connection_object = {
-            'group.id': GROUP_ID,
-            'bootstrap.servers': INSIGHTS_KAFKA_ADDRESS,
-            'enable.auto.commit': False
-    }
-    producer = Producer(kafka_auth_config(connection_object))
-
+    producer = Producer(kafka_auth_config())
     return producer
