eca110125ec6bc305f4f1f68b15503e8b4b8cc26
Rohini Chandra <61837065+r14chandra@users.noreply.github.com>
2024-11-26 16:05:27 +0530

    keep other clowdapp dependencies intact to keep IQE tests running

--- ros/processor/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-
-def create_app():
-    app = Flask(__name__)
-    return app
-
-
-app = create_app()
--- ros/processor/pcp_generator.py
+++ ros/processor/pcp_generator.py
@@ -1,5 +1,6 @@
-from ros.processor import app
+import time
 from ros.lib.config import get_logger
+from prometheus_client import start_http_server
 from ros.lib.config import ROS_PCP_PROCESSOR_PORT
 
 
@@ -10,7 +11,16 @@ class PCPGenerator:
     def __init__(self):
         pass
 
+    def run(self):
+        try:
+            logging.info("Flask server running on port %s", ROS_PCP_PROCESSOR_PORT)
+            while True:
+                time.sleep(1)
+        except Exception as err:
+            logging.error(err)
+
 
 if __name__ == "__main__":
-    logging.info("Flask server running on port %s", ROS_PCP_PROCESSOR_PORT)
-    app.run(host='0.0.0.0', port=ROS_PCP_PROCESSOR_PORT)
+    start_http_server(ROS_PCP_PROCESSOR_PORT)
+    processor = PCPGenerator()
+    processor.run()
