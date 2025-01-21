82e67646725159e50dcc1ae2a4f67b4b6cc2929c
Rohini Chandra <61837065+r14chandra@users.noreply.github.com>
2025-01-21 17:55:17 +0530

    RHINENG-13973 execute pcp commands

--- ros/processor/suggestions_engine.py
+++ ros/processor/suggestions_engine.py
@@ -1,15 +1,21 @@
+import os
 import json
-import requests
+import shutil
+import subprocess
 from http import HTTPStatus
-from ros.lib import consume
-from insights import extract
-from ros.lib.config import get_logger
+from contextlib import contextmanager
 from tempfile import NamedTemporaryFile
+
+import requests
+from insights import extract
 from prometheus_client import start_http_server
+
+from ros.lib import consume
 from ros.lib.config import (
-    INVENTORY_EVENTS_TOPIC,
+    get_logger,
     METRICS_PORT,
-    GROUP_ID_SUGGESTIONS_ENGINE
+    INVENTORY_EVENTS_TOPIC,
+    GROUP_ID_SUGGESTIONS_ENGINE,
 )
 
 
@@ -22,6 +28,143 @@ class SuggestionsEngine:
         self.service = 'SUGGESTIONS_ENGINE'
         self.event = None
 
+    def run_pmlogextract(self, host, index_file_path, output_dir):
+        """Run the pmlogextract command."""
+
+        pmlogextract_command = [
+            "pmlogextract",
+            "-c",
+            "ros/lib/pcp_extract_config",
+            index_file_path,
+            output_dir
+        ]
+
+        logging.debug(f"{self.service} - {self.event} - Running pmlogextract command for system {host.get('id')}.")
+        try:
+            subprocess.run(pmlogextract_command, check=True)
+            logging.debug(
+                f"{self.service} - {self.event} - Successfully ran pmlogextract command for system {host.get('id')}."
+            )
+        except subprocess.CalledProcessError as error:
+            logging.error(
+                f"{self.service} - {self.event} - Error running pmlogextract command for system {host.get('id')}:"
+                f" {error.stdout}"
+            )
+            raise
+
+    def run_pmlogsummary(self, host, output_dir):
+        """Run the pmlogsummary command."""
+
+        pmlogsummary_command = [
+            "pmlogsummary",
+            "-F",
+            os.path.join(output_dir, ".index")
+        ]
+
+        logging.debug(f"{self.service} - {self.event} - Running pmlogsummary command for system {host.get('id')}.")
+        try:
+            subprocess.run(pmlogsummary_command, check=True, text=True, capture_output=True)
+            logging.debug(
+                f"{self.service} - {self.event} - Successfully ran pmlogsummary command for system {host.get('id')}."
+            )
+        except subprocess.CalledProcessError as error:
+            logging.error(
+                f"{self.service} - {self.event} - Error running pmlogsummary command for system {host.get('id')}:"
+                f" {error.stdout}"
+            )
+            raise
+
+    def create_output_dir(self, request_id, host):
+        output_dir = f"/var/tmp/pmlogextract-output-{request_id}/"
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+
+        logging.debug(f"Successfully created output_dir for system {host.get('id')}: {output_dir}")
+
+        return output_dir
+
+    def run_pcp_commands(self, host, index_file_path, request_id):
+        sanitized_request_id = request_id.replace("/", "_")
+        output_dir = self.create_output_dir(sanitized_request_id, host)
+
+        self.run_pmlogextract(host, index_file_path, output_dir)
+
+        self.run_pmlogsummary(host, output_dir)
+
+        logging.info(f"{self.service} - {self.event} - Successfully ran pcp commands for system {host.get('id')}.")
+
+        try:
+            if os.path.exists(output_dir):
+                shutil.rmtree(output_dir)
+                logging.debug(
+                    f"{self.service} - {self.event} - Cleaned up output directory for system {host.get('id')}."
+                )
+        except Exception as error:
+            logging.error(
+                f"{self.service} - {self.event} - Error cleaning up the output directory for system {host.get('id')}:"
+                f" {error}"
+            )
+
+    def find_root_directory(self, directory, target_file):
+        """
+        Recursively search for the target file in the given directory - to get root directory of an archive.
+        """
+        for dirpath, _, filenames in os.walk(directory):
+            if target_file in filenames:
+                return dirpath
+
+        return None
+
+    def get_index_file_path(self, host, extracted_dir):
+        extracted_dir_root = self.find_root_directory(extracted_dir, "insights_archive.txt")
+
+        if not extracted_dir_root:
+            logging.error(
+                f"{self.service} - {self.event} -"
+                f" insights_archive.txt not found in the extracted dir for system {host.get('id')}"
+            )
+            return None
+
+        pmlogger_dir = os.path.join(extracted_dir_root, "data/var/log/pcp/pmlogger/")
+
+        index_files = [
+            file for file in os.listdir(pmlogger_dir) if file.endswith(".index")
+        ]
+        index_file_path = os.path.join(pmlogger_dir, index_files[0])
+
+        return index_file_path
+
+    @contextmanager
+    def download_and_extract(self, archive_URL, host, org_id):
+        logging.debug(f"{self.service} - {self.event} - Report downloading for system {host.get('id')}.")
+
+        try:
+            response = requests.get(archive_URL, timeout=10)
+
+            if response.status_code != HTTPStatus.OK:
+                logging.error(
+                    f"{self.service} - {self.event} - Unable to download the report for system {host.get('id')}. "
+                    f"ERROR - {response.reason}"
+                )
+                yield None
+            else:
+                with NamedTemporaryFile(delete=True) as tempfile:
+                    tempfile.write(response.content)
+                    logging.info(
+                        f"{self.service} - {self.event} - Report downloaded successfully for system {host.get('id')}"
+                    )
+                    tempfile.flush()
+                    with extract(tempfile.name) as extract_dir:
+                        yield extract_dir
+        except Exception as error:
+            logging.error(f"{self.service} - {self.event} - Error occurred during download and extraction: {error}")
+
+    def is_pcp_collected(self, platform_metadata):
+        return (
+            platform_metadata.get('is_ros_v2') and
+            platform_metadata.get('is_pcp_raw_data_collected')
+        )
+
     def handle_create_update(self, payload):
         self.event = "Update event" if payload.get('type') == 'updated' else "Create event"
 
@@ -32,11 +175,19 @@ class SuggestionsEngine:
             logging.info(f"{self.service} - {self.event} - Missing host or/and platform_metadata field(s).")
             return
 
-        if not is_pcp_collected(platform_metadata):
+        if not self.is_pcp_collected(platform_metadata):
             return
 
         archive_URL = platform_metadata.get('url')
-        download_and_extract(self.service, self.event, archive_URL, host, org_id=host.get('org_id'))
+        with self.download_and_extract(
+                archive_URL,
+                host,
+                org_id=host.get('org_id')
+        ) as ext_dir:
+            extracted_dir = ext_dir.tmp_dir
+            index_file_path = self.get_index_file_path(host, extracted_dir)
+            if index_file_path is not None:
+                self.run_pcp_commands(host, index_file_path, platform_metadata.get('request_id'))
 
     def run(self):
         logging.info(f"{self.service} - Engine is running. Awaiting msgs.")
@@ -52,44 +203,17 @@ class SuggestionsEngine:
 
                     if 'created' == event_type or 'updated' == event_type:
                         self.handle_create_update(payload)
-                        self.consumer.commit()
 
                 except json.JSONDecodeError as error:
                     logging.error(f"{self.service} - {self.event} - Failed to decode message: {error}")
                 except Exception as error:
                     logging.error(f"{self.service} - {self.event} - Error processing message: {error}")
+                finally:
+                    self.consumer.commit()
         except Exception as error:
             logging.error(f"{self.service} - {self.event} - error: {error}")
         finally:
             self.consumer.close()
-
-
-def download_and_extract(service, event, archive_URL, host, org_id):
-    logging.info(f"{service} - {event} - Downloading the report for system {host.get('id')}.")
-
-    response = requests.get(archive_URL, timeout=10)
-
-    if response.status_code != HTTPStatus.OK:
-        logging.error(
-            f"{service} - {event} - Unable to download the report for system {host.get('id')}. "
-            f"ERROR - {response.reason}"
-        )
-    else:
-        with NamedTemporaryFile(delete=True) as tempfile:
-            tempfile.write(response.content)
-            logging.info(
-                f"{service} - {event} - Downloaded the report successfully for system {host.get('id')}"
-            )
-            tempfile.flush()
-            with extract(tempfile.name) as extract_dir:
-                return extract_dir.tmp_dir
-
-
-def is_pcp_collected(platform_metadata):
-    return (
-        platform_metadata.get('is_ros_v2') and
-        platform_metadata.get('is_pcp_raw_data_collected')
-    )
 
 
 if __name__ == "__main__":
--- tests/test_suggestions_engine.py
+++ tests/test_suggestions_engine.py
@@ -1,11 +1,8 @@
 import unittest
 import logging
 from unittest.mock import patch
-from ros.processor.suggestions_engine import (
-    SuggestionsEngine,
-    is_pcp_collected,
-    download_and_extract
-)
+
+from ros.processor.suggestions_engine import SuggestionsEngine
 
 
 class TestSuggestionsEngine(unittest.TestCase):
@@ -33,19 +30,21 @@ class TestSuggestionsEngine(unittest.TestCase):
 
     def test_is_pcp_collected(self):
         valid_metadata = {'is_ros_v2': True, 'is_pcp_raw_data_collected': True}
-        self.assertTrue(is_pcp_collected(valid_metadata))
+        self.assertTrue(self.engine.is_pcp_collected(valid_metadata))
 
         invalid_metadata = {'is_ros_v2': True, 'is_pcp_raw_data_collected': False}
-        self.assertFalse(is_pcp_collected(invalid_metadata))
+        self.assertFalse(self.engine.is_pcp_collected(invalid_metadata))
 
         invalid_metadata = {'is_ros_v2': False, 'is_pcp_raw_data_collected': True}
-        self.assertFalse(is_pcp_collected(invalid_metadata))
+        self.assertFalse(self.engine.is_pcp_collected(invalid_metadata))
 
         invalid_metadata = {'is_ros_v2': False, 'is_pcp_raw_data_collected': False}
-        self.assertFalse(is_pcp_collected(invalid_metadata))
+        self.assertFalse(self.engine.is_pcp_collected(invalid_metadata))
 
 
 class TestDownloadAndExtract(unittest.TestCase):
+    def setUp(self):
+        self.engine = SuggestionsEngine()
 
     @patch("ros.processor.suggestions_engine.extract")
     @patch("ros.processor.suggestions_engine.NamedTemporaryFile")
@@ -58,18 +57,16 @@ class TestDownloadAndExtract(unittest.TestCase):
 
         mock_extract.return_value.__enter__.return_value.tmp_dir = "extracted_dir"
 
-        extract_dir = download_and_extract(
-            service="TestService",
-            event="TestEvent",
-            archive_URL="http://example.com/archive.tar.gz",
-            host={"id": "test_host"},
-            org_id="123"
-        )
+        with self.engine.download_and_extract(
+                archive_URL="http://example.com/archive.tar.gz",
+                host={"id": "test_host"},
+                org_id="123"
+        ) as extract_dir:
+            self.assertEqual(extract_dir.tmp_dir, "extracted_dir")
 
         mock_get.assert_called_once_with("http://example.com/archive.tar.gz", timeout=10)
         mock_tempfile.return_value.__enter__.assert_called_once()
         mock_extract.assert_called_once()
-        self.assertEqual(extract_dir, "extracted_dir")
 
     @patch('ros.processor.suggestions_engine.extract')
     @patch('ros.processor.suggestions_engine.requests.get')
@@ -79,16 +76,81 @@ class TestDownloadAndExtract(unittest.TestCase):
 
         mock_extract.return_value.__enter__.return_value.tmp_dir = "extracted_dir"
 
-        extract_dir = download_and_extract(
-            service="TestService",
-            event="TestEvent",
-            archive_URL="http://example.com/archive.tar.gz",
-            host={"id": "test_host"},
-            org_id="123"
-        )
+        with self.engine.download_and_extract(
+                archive_URL="http://example.com/archive.tar.gz",
+                host={"id": "test_host"},
+                org_id="123"
+        ) as extract_dir:
+            self.assertIsNone(extract_dir)
 
         mock_get.assert_called_once_with("http://example.com/archive.tar.gz", timeout=10)
-        self.assertIsNone(extract_dir)
+
+
+class TestFindRootDirectory(unittest.TestCase):
+    def setUp(self):
+        self.engine = SuggestionsEngine()
+
+    @patch('ros.processor.suggestions_engine.os.walk')
+    def test_file_found_in_root_directory(self, mock_walk):
+        mock_walk.return_value = [
+            ("/root", ["subdir1", "subdir2"], ["other_file.txt"]),
+            ("/root/subdir1", [], ["insights_archive.txt"]),
+            ("/root/subdir2", [], ["file2.txt"]),
+        ]
+
+        result = self.engine.find_root_directory("/root", "insights_archive.txt")
+        self.assertEqual(result, "/root/subdir1")
+
+    @patch('ros.processor.suggestions_engine.os.walk')
+    def test_file_not_found(self, mock_walk):
+        mock_walk.return_value = [
+            ("/root", ["subdir1", "subdir2"], ["other_file.txt"]),
+            ("/root/subdir1", [], ["file1.txt"]),
+            ("/root/subdir2", [], ["file2.txt"]),
+        ]
+
+        result = self.engine.find_root_directory("/root", "insights_archive.txt")
+        self.assertIsNone(result)
+
+
+class TestGetIndexFilePath(unittest.TestCase):
+    def setUp(self):
+        self.engine = SuggestionsEngine()
+
+    @patch("ros.processor.suggestions_engine.os.listdir")
+    @patch("ros.processor.suggestions_engine.os.path.join")
+    @patch("ros.processor.suggestions_engine.SuggestionsEngine.find_root_directory")
+    def test_get_index_file_path(self, mock_find_root_directory, mock_join, mock_listdir):
+        mock_find_root_directory.return_value = "/tmp/extracted/"
+
+        mock_listdir.return_value = ["YYYYMMDD.index"]
+        mock_join.return_value = "/var/tmp/extracted/data/var/log/pcp/pmlogger/YYYYMMDD.index"
+
+        index_file_path = self.engine.get_index_file_path(
+            host={"id": "test_host"},
+            extracted_dir="/tmp/extracted"
+        )
+
+        mock_listdir.assert_called_once_with("/var/tmp/extracted/data/var/log/pcp/pmlogger/YYYYMMDD.index")
+        self.assertEqual(index_file_path, "/var/tmp/extracted/data/var/log/pcp/pmlogger/YYYYMMDD.index")
+
+
+class TestCreateOutputDir(unittest.TestCase):
+    def setUp(self):
+        self.engine = SuggestionsEngine()
+
+    @patch("ros.processor.suggestions_engine.os.makedirs")
+    @patch("ros.processor.suggestions_engine.os.path.exists")
+    def test_create_output_dir(self, mock_path_exists, mock_makedirs):
+        request_id = "12345"
+        host = {"id": "test_host"}
+        mock_path_exists.return_value = False
+        mock_makedirs.return_value = None
+
+        output_dir = self.engine.create_output_dir(request_id, host)
+
+        mock_makedirs.assert_called_once_with(output_dir)
+        self.assertEqual(output_dir, "/var/tmp/pmlogextract-output-12345/")
 
 
 if __name__ == '__main__':
