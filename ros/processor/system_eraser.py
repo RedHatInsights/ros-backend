44659b84aad43de0ece0c6c16634722853fb6389
Rohini Chandra <61837065+r14chandra@users.noreply.github.com>
2025-08-26 18:25:09 +0530

    RHINENG-20267 Implement Kafka consumer for system eraser service

---
+++ ros/processor/system_eraser.py
@@ -0,0 +1,59 @@
+import json
+
+from prometheus_client import start_http_server
+
+from ros.lib.config import (
+    get_logger,
+    METRICS_PORT,
+    INVENTORY_EVENTS_TOPIC,
+    GROUP_ID_SUGGESTIONS_ENGINE,
+)
+from ros.lib import consume
+
+
+logging = get_logger(__name__)
+
+
+class SystemEraser:
+    def __init__(self):
+        self.consumer = consume.init_consumer(INVENTORY_EVENTS_TOPIC, GROUP_ID_SUGGESTIONS_ENGINE)
+        self.service = 'SYSTEM_ERASER'
+        self.event = 'Delete event'
+
+    def run(self):
+        logging.info(f"{self.service} - System Eraser is running. Awaiting msgs.")
+        try:
+            while True:
+                message = self.consumer.poll(timeout=1.0)
+                if message is None:
+                    continue
+
+                try:
+                    payload = json.loads(message.value().decode('utf-8'))
+                    event_type = payload['type']
+
+                    if event_type != 'delete':
+                        continue
+
+                    host = payload.get('host')
+                    host_id = host.get('id')
+
+                    logging.debug(
+                        f"{self.service} - Received a message for system with inventory_id {host_id}"
+                    )
+
+                except json.JSONDecodeError as error:
+                    logging.error(f"{self.service} - {self.event} - Failed to decode message: {error}")
+                except Exception as error:
+                    logging.error(f"{self.service} - {self.event} - Error processing message: {error}")
+
+        except Exception as error:
+            logging.error(f"{self.service} - {self.event} - error: {error}")
+        finally:
+            self.consumer.close()
+
+
+if __name__ == "__main__":
+    start_http_server(int(METRICS_PORT))
+    processor = SystemEraser()
+    processor.run()
