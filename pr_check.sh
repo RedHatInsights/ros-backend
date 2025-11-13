7ee6d49f4c802e71df0ef1d2433f4abc30aae468
Saurabh Pimpalgaokar <spimpalg@spimpalg-thinkpadt14gen4.punetw6.csb>
2025-11-13 18:10:01 +0530

    modified pr checks

--- pr_check.sh
+++ pr_check.sh
@@ -9,11 +9,26 @@ export COMPONENTS="ros-backend"
 export IMAGE="quay.io/cloudservices/ros-backend"
 export DOCKERFILE="Dockerfile"
 
+# NEW: Backend version selection logic
+BACKEND_VERSION=${BACKEND_VERSION:-"old"}
+
+if [ "$BACKEND_VERSION" = "old" ]; then
+    export IQE_MARKER_EXPRESSION="ros_smoke and (common or old_backend)"
+    export IBUTSU_SOURCE="ros-old-backend-pr"
+elif [ "$BACKEND_VERSION" = "new" ]; then
+    export IQE_MARKER_EXPRESSION="ros_smoke and (common or new_backend)"
+    export IBUTSU_SOURCE="ros-new-backend-pr"
+else
+    # Default fallback
+    export IQE_MARKER_EXPRESSION="ros_smoke"
+    export IBUTSU_SOURCE="ros-backend-pr"
+fi
+
 export IQE_PLUGINS="ros"
-export IQE_MARKER_EXPRESSION="ros_smoke"
 export IQE_FILTER_EXPRESSION=""
 export IQE_CJI_TIMEOUT="30m"
 export IQE_ENV_VARS="JOB_NAME=${JOB_NAME},BUILD_NUMBER=${BUILD_NUMBER},BUILD_URL=${BUILD_URL}"
+export EXTRA_DEPLOY_ARGS="--set-parameter ros-backend/BACKEND_VERSION=${BACKEND_VERSION}"
 
 # Install bonfire repo/initialize
 CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
